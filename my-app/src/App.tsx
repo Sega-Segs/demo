3e8e0c5f9fbc8e4b343644fee21113e9fe677278
Sega-Segs <egeinanc@hotmail.com>
2022-09-12 22:07:33 +0200

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BasicAlerts from './Button-app';
+import BasicTabs from './jlk';
+
+function App() {
+  return (
+    <div className="App">
+       
+        <BasicTabs></BasicTabs>
+      
+    </div>
+  );
+}
+
+export default App;
